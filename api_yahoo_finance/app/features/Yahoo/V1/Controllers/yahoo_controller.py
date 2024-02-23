5cd5a24e2a90d518f8d28a0bf5b30014b8101b90
Eduardooliveira95 <eduardooliveirafo@sptech.school>
2024-02-23 17:52:27 -0300

    Evolução do CRUD

--- api_yahoo_finance/app/features/Yahoo/V1/Controllers/yahoo_controller.py
+++ api_yahoo_finance/app/features/Yahoo/V1/Controllers/yahoo_controller.py
@@ -1,17 +1,20 @@
+from pathlib import Path
+
 from fastapi import APIRouter, HTTPException
 from typing import List
+from fastapi.responses import FileResponse
 from app.features.Yahoo.V1.Models.Request.DadosUsuarioRequest import DadosUsuarioRequest
 from app.features.Yahoo.V1.Models.Response.DadosResponse import DadosResponse
 from app.features.Yahoo.V1.Services.yahoo_service import YahooService
 
-
 router = APIRouter()
 yahoo_service = YahooService()
 
-@router.post("/dados", response_model=List[DadosResponse])
+@router.get("/dadosFechamento", response_model=List[DadosResponse])
 async def buscar_dados(dados: DadosUsuarioRequest, skip: int = 0, limit: int = 10):
-    return yahoo_service.chamarAPiYahoo(dados, skip=skip, limit=limit)
+    return yahoo_service.buscarDadosFechamentoAtivos(dados, skip=skip, limit=limit)
 
-#@router.get("/users/", response_model=List[UserUpdate])
-#async def read_users(skip: int = 0, limit: int = 10):
-#    return user_service.get_users(skip=skip, limit=limit)
+@router.get("/dadosFechamento/excel")
+async def buscar_dados_excel(dados : DadosUsuarioRequest, skip : int = 0, limit : int = 10):
+    excel = yahoo_service.buscarDadosFechamentoAtivosExcel(dados, skip=skip, limit=limit)
+    return FileResponse(path=excel, media_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
--- api_yahoo_finance/app/features/Yahoo/V1/Services/yahoo_service.py
+++ api_yahoo_finance/app/features/Yahoo/V1/Services/yahoo_service.py
@@ -6,6 +6,8 @@ import matplotlib.pyplot as plt
 from datetime import date, datetime, timedelta
 import yfinance as yf
 import warnings
+import pandas as pd
+from pathlib import Path
 
 from app.features.Yahoo.V1.Models.DTO.IndicadoresAtivo import IndicadoresAtivo
 from app.features.Yahoo.V1.Models.Request.DadosUsuarioRequest import DadosUsuarioRequest
@@ -13,9 +15,11 @@ from app.features.Yahoo.V1.Models.Response.DadosResponse import DadosResponse
 
 warnings.filterwarnings("ignore", category=FutureWarning)
 
+
 class YahooService:
 
-    def chamarAPiYahoo(self, dados: DadosUsuarioRequest, null=None, skip: int = 0, limit: int = 100) -> List[DadosResponse]:
+    def buscarDadosFechamentoAtivos(self, dados: DadosUsuarioRequest, null=None, skip: int = 0, limit: int = 100) -> \
+    List[DadosResponse]:
         lista_de_ativos = dados.ativos
         dados: List[DadosResponse] = []
         for ativo in lista_de_ativos:
@@ -27,8 +31,12 @@ class YahooService:
             for valor, data in zip(fechamentosBolsa, dataAcao):
                 indicador = IndicadoresAtivo(data=data, valorFechamentoDia=valor)
                 dadosBolsa.indicadores.append(indicador)
-
             dados.append(dadosBolsa)
-
-
         return dados
+
+    def buscarDadosFechamentoAtivosExcel(self, dados, skip, limit) -> str:
+        buscarDados = self.buscarDadosFechamentoAtivos(dados)
+        df = pd.DataFrame(buscarDados)
+        dadosFechamentoBolsa = Path.home() / "Downloads" / "dados_fechamento_bolsa.xlsx"
+        df.to_excel(dadosFechamentoBolsa, index=False)
+        return str(dadosFechamentoBolsa)
